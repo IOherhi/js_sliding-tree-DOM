a35f2c22419a2bac58f71d140b1c8d695187df78
Ivan Herhi <ionherhi@gmail.com>
2025-03-13 14:08:41 +0100

    SS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const tree = document.body.querySelector('tree')
+const tree = document.body.querySelector('.tree');
 const childrens = tree.children;
 
 const Ul01 = childrens[0].querySelector('ul');
@@ -9,7 +9,6 @@ const Ul02 = childrens[1].querySelector('ul');
 const Urray = Array.from(Ul01.children).concat(Array.from(Ul02.children));
 
 Urray.forEach((item) => {
-
   const content = item.textContent;
   const span = document.createElement('span');
 
@@ -17,15 +16,15 @@ Urray.forEach((item) => {
   item.textContent = '';
   item.prepend(span);
 
-  span.addEventListener('click', (e) => {
-    const workArray = Array.from(item.children).slice(1, -1);
+  span.addEventListener('click', () => {
+    const workArray = Array.from(item.children).slice(1);
 
     workArray.forEach((i) => {
-        if (i.style.display === 'none') {
-          i.style.display= 'block';
-        } else {
-          i.style.display = 'none';
-        }
-    })
-  })
+      if (i.style.display === 'none') {
+        i.style.display = 'block';
+      } else {
+        i.style.display = 'none';
+      }
+    });
+  });
 });
