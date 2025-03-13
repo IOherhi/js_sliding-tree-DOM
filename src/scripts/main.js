dbba06f4bb08f5f2f8fe912f78cff04f0ab7cdec
Ivan Herhi <ionherhi@gmail.com>
2025-03-13 10:35:27 +0100

    sliding01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-// write code here
+const tree = document.body.querySelector('tree')
+const childrens = tree.children;
+
+const Ul01 = childrens[0].querySelector('ul');
+const Ul02 = childrens[1].querySelector('ul');
+
+const Ul01Array = Array.from(Ul01.children);
+
+Ul01Array.forEach((item) =>
+  item.onclick((e) => {
+    Array.from(e.currentTarget.children).forEach((i) => {
+        if (i.style.display === 'none') {
+          i.setAttribute('display', 'block');
+        } else {
+          i.style.display = 'none';
+        }
+    })
+  })
+);
